7437bac62d39921f4e90682ff9d848ece017a7a5
Eleazar Justo <EleazarJ@magenic.com>
2020-09-29 15:24:11 +0800

    Dungeons screen

--- src/views/Dungeons.vue
+++ src/views/Dungeons.vue
@@ -3,9 +3,45 @@
     <div class="container">
       <div class="row nowrap dungeons-section">
         <div class="col-sm-6 dungeon-list">
-          <div v-for="dungeon in dungeons" :key="dungeon._id">{{dungeon.name}}</div>
+          <div
+            class="dungeon-tile"
+            v-for="dungeon in dungeons"
+            :key="dungeon._id"
+            @click="getDungeon(dungeon._id)"
+            :style="{'background-image': `url(${getImage(dungeon.image)})`}"
+          >
+            <img
+              v-if="dungeon.recommendedLevel > level"
+              src="../assets/dungeons/locked-overlay.png"
+            />
+          </div>
         </div>
-        <div class="col-sm-6">test</div>
+        <div class="col-sm-6">
+          <div v-if="selectedDungeon">
+            <div class="row">
+              <div class="col-sm-12 pad-1 title">{{ selectedDungeon.name }}</div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 bold left-text">Recommended Level:</div>
+              <div class="col-sm-4 left-text">{{ selectedDungeon.recommendedLevel }}</div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12 bold left-text">Enemies/Drops</div>
+            </div>
+            <div v-for="enemy in selectedDungeon.enemies" :key="enemy._id">
+              <div class="row">
+                <div class="col-sm-4 bold">{{enemy.name}}</div>
+              </div>
+              <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-8 text-left" v-if="enemy.drops && enemy.drops.length">
+                  <div class="col-sm-8" v-for="drop in enemy.drops" :key="drop._id">* {{drop.name}}</div>
+                </div>
+                <div v-else class="col-sm-8 text-left">-----</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -14,10 +50,17 @@
 import { GET_DUNGEONS } from '../store/actions.type';
 export default {
   name: 'Dungeons',
+  data() {
+    return {
+      selectedDungeon: {}
+    };
+  },
   created() {
     this.$store
       .dispatch(GET_DUNGEONS, this.$store.state.character.details._id)
-      .then(response => console.log(response, 'dungeons'));
+      .then(response => {
+        if (response && response.length) this.selectedDungeon = response[0];
+      });
   },
   computed: {
     characterId() {
@@ -25,6 +68,21 @@ export default {
     },
     dungeons() {
       return this.$store.state.character.dungeons;
+    },
+    level() {
+      return this.$store.state.character.details.level;
+    }
+  },
+  methods: {
+    getDungeon(id) {
+      this.selectedDungeon =
+        this.dungeons &&
+        this.dungeons.find(function(dungeon) {
+          return dungeon._id === id;
+        });
+    },
+    getImage: function(image) {
+      return require(`../assets/dungeons/${image}.jpg`);
     }
   }
 };
@@ -53,6 +111,19 @@ export default {
   border-right: 5px solid #d39e0033;
   height: 650px;
   overflow-y: auto;
+}
+
+.dungeon-tile {
+  cursor: pointer;
+  width: 380px;
+  height: 280px;
+  margin: 1rem auto;
+  background-size: cover;
+}
+
+img {
+  width: 100%;
+  height: 100%;
 }
 
 /* width */
